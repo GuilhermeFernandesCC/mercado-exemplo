efdfe0bb2b5210a511bfcadad83cda47c7968fff
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2022-05-11 17:29:00 -0300

    ProdutoRepository

---
+++ src/Repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+import exemplo.Produto;
+
+import java.util.HashMap;
+
+public class ProdutoRepository {
+    HashMap catalogo = new HashMap<String, Produto>;
+
+}
+public addProduto(Produto p){
+
+}
